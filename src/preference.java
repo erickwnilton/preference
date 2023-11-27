557bb9cf992351ff607694241d1f3ed16418ee68
23eerick <erickwelberth1@hotmail.com>
2023-11-27 10:59:57 -0300

    structure

--- src/preference.java
+++ src/preference.java
@@ -1,6 +1,10 @@
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 class Person {
     String name;
-    int age;
+    static int age;
     String hometown;
 
 public Person(String name, int age, String hometown) {
@@ -12,6 +16,45 @@ public Person(String name, int age, String hometown) {
 
 public class preference {
         public static void main(String[] args) {
-            System.out.printf("Hello and welcome!");
-    }
+            Queue<String> row = new LinkedList<>();
+            Scanner Data = new Scanner(System.in);
+
+            while (true) {
+                System.out.println("1. Adicionar novo cliente");
+                System.out.println("2. Listar clientes preferenciais");
+                System.out.println("3. Atualizar cliente");
+                System.out.println("4. Remover cliente");
+                System.out.println("0. Sair");
+
+                int readOption = Data.nextInt();
+
+                switch (readOption) {
+                    case 1 -> {
+
+                        break;
+                    }
+                    case 2 -> {
+
+                        break;
+                    }
+                    case 3 -> {
+
+                        break;
+                    }
+                    case 4 -> {
+
+                        break;
+                    }
+                    case 0 -> {
+                        Data.close();
+                        System.exit(0);
+                    }
+                    default -> {
+                        System.out.println("Opção inválida.");
+                        System.out.println("Digite uma das opções abaixo:");
+                        break;
+                    }
+                }
+            }
+        }
 }
