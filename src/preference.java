28d497491e216b0ba6f6f6d8bd8d54a0a5b0cb88
23eerick <erickwelberth1@hotmail.com>
2023-11-27 08:09:36 -0300

    craft class

---
+++ src/preference.java
@@ -0,0 +1,17 @@
+class Person {
+    String name;
+    int age;
+    String hometown;
+
+public Person(String name, int age, String hometown) {
+    this.name = name;
+    this.age = age;
+    this.hometown = hometown;
+ }
+}
+
+public class preference {
+        public static void main(String[] args) {
+            System.out.printf("Hello and welcome!");
+    }
+}
